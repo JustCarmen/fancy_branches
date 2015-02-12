20d1e288013ce9584c3500fa61b102335442ce97
JustCarmen <carmen@justcarmen.nl>
2015-02-12 15:01:21 +0100

    use webtrees function

--- module.php
+++ module.php
@@ -100,7 +100,8 @@ class fancy_branches_WT_Module extends Module implements ModuleConfigInterface, 
 				<?php
 				break;
 			default:
-				header('HTTP/1.0 404 Not Found');
+				http_response_code(404);
+				break;
 		}
 	}
 
