5726bab615c3a896c3673ce71d6603cd6b0762ed
JustCarmen <carmen@justcarmen.nl>
2017-06-26 21:11:36 +0200

    Source formatting. Convert tabs to spaces

--- app/Template/AdminTemplate.php
+++ app/Template/AdminTemplate.php
@@ -25,48 +25,48 @@ use JustCarmen\WebtreesAddOns\FancyBranches\FancyBranchesModule;
 
 class AdminTemplate extends FancyBranchesModule {
 
-	protected function pageContent() {
-		$controller = new PageController;
-		return
-			$this->pageHeader($controller) .
-			$this->pageBody($controller);
-	}
+  protected function pageContent() {
+    $controller = new PageController;
+    return
+        $this->pageHeader($controller) .
+        $this->pageBody($controller);
+  }
 
-	private function pageHeader(PageController $controller) {
-		$controller
-			->restrictAccess(Auth::isAdmin())
-			->setPageTitle($this->getTitle())
-			->pageHeader();
-	}
+  private function pageHeader(PageController $controller) {
+    $controller
+        ->restrictAccess(Auth::isAdmin())
+        ->setPageTitle($this->getTitle())
+        ->pageHeader();
+  }
 
-	private function pageBody(PageController $controller) {
+  private function pageBody(PageController $controller) {
 
-		echo Bootstrap4::breadcrumbs([
-			'admin.php'			 => I18N::translate('Control panel'),
-			'admin_modules.php'	 => I18N::translate('Module administration'),
-			], $controller->getPageTitle());
-		?>
+    echo Bootstrap4::breadcrumbs([
+        'admin.php'         => I18N::translate('Control panel'),
+        'admin_modules.php' => I18N::translate('Module administration'),
+        ], $controller->getPageTitle());
+    ?>
 
-		<h1><?= $controller->getPageTitle() ?></h1>
-		<form method="post" name="form1">
-			<?= Filter::getCsrf() ?>
-			<input type="hidden" name="save" value="1">
-			<div class="row form-group">
-				<label class="col-sm-4">
-					<?= I18N::translate('Use “d’Aboville” numbering system') ?>
-				</label>
-				<div class="col-sm-8">
-					<?php $FB = $this->getPreference('FB'); ?>
-					<?= Bootstrap4::radioButtons('NEW_FB', FunctionsEdit::optionsNoYes(), $FB, true) ?>
-					<p class="small text-muted"><?= I18N::translate('The “D’aboville” numbering system is a method to split descending generations into numbering sections. Each generation and each child gets a succeeding number seperated by a dot.') ?></p>
-				</div>
-			</div>
-			<button class="btn btn-primary" type="submit">						
-				<i class="fa fa-check"></i>
-				<?= I18N::translate('save') ?>
-			</button>
-		</form>
-		<?php
-	}
+    <h1><?= $controller->getPageTitle() ?></h1>
+    <form method="post" name="form1">
+      <?= Filter::getCsrf() ?>
+      <input type="hidden" name="save" value="1">
+      <div class="row form-group">
+        <label class="col-sm-4">
+          <?= I18N::translate('Use “d’Aboville” numbering system') ?>
+        </label>
+        <div class="col-sm-8">
+          <?php $FB = $this->getPreference('FB'); ?>
+          <?= Bootstrap4::radioButtons('NEW_FB', FunctionsEdit::optionsNoYes(), $FB, true) ?>
+          <p class="small text-muted"><?= I18N::translate('The “D’aboville” numbering system is a method to split descending generations into numbering sections. Each generation and each child gets a succeeding number seperated by a dot.') ?></p>
+        </div>
+      </div>
+      <button class="btn btn-primary" type="submit">
+        <i class="fa fa-check"></i>
+        <?= I18N::translate('save') ?>
+      </button>
+    </form>
+    <?php
+  }
 
 }
--- module.php
+++ module.php
@@ -27,72 +27,72 @@ use JustCarmen\WebtreesAddOns\FancyBranches\Template\AdminTemplate;
 
 class FancyBranchesModule extends AbstractModule implements ModuleConfigInterface, ModuleMenuInterface {
 
-	const CUSTOM_VERSION	 = '1.7.11';
-	const CUSTOM_WEBSITE	 = 'http://www.justcarmen.nl/fancy-modules/fancy-branches/';
+  const CUSTOM_VERSION = '1.8.0-dev';
+  const CUSTOM_WEBSITE = 'http://www.justcarmen.nl/fancy-modules/fancy-branches/';
 
-	/** @var string location of the fancy treeview module files */
-	var $directory;
+  /** @var string location of the fancy treeview module files */
+  var $directory;
 
-	public function __construct() {
-		parent::__construct('fancy_branches');
+  public function __construct() {
+    parent::__construct('fancy_branches');
 
-		$this->directory = WT_STATIC_URL . WT_MODULES_DIR . $this->getName();
+    $this->directory = WT_STATIC_URL . WT_MODULES_DIR . $this->getName();
 
-		// register the namespaces
-		$loader = new ClassLoader();
-		$loader->addPsr4('JustCarmen\\WebtreesAddOns\\FancyBranches\\', WT_MODULES_DIR . $this->getName() . '/app');
-		$loader->register();
-	}
+    // register the namespaces
+    $loader = new ClassLoader();
+    $loader->addPsr4('JustCarmen\\WebtreesAddOns\\FancyBranches\\', WT_MODULES_DIR . $this->getName() . '/app');
+    $loader->register();
+  }
 
-	// Extend Module
-	public function getTitle() {
-		return /* I18N: Name of a module */ I18N::translate('Fancy Branches');
-	}
+  // Extend Module
+  public function getTitle() {
+    return /* I18N: Name of a module */ I18N::translate('Fancy Branches');
+  }
 
-	// Extend Module
-	public function getDescription() {
-		return
-			/* I18N: Description of the module */ I18N::translate('Expand or collapse branches in the webtrees branches list with a single click.');
-	}
+  // Extend Module
+  public function getDescription() {
+    return
+        /* I18N: Description of the module */ I18N::translate('Expand or collapse branches in the webtrees branches list with a single click.');
+  }
 
-	// Extend ModuleConfigInterface
-	public function modAction($mod_action) {
-		switch ($mod_action) {
-			case 'admin_config':
-				if (Filter::postBool('save') && Filter::checkCsrf()) {
-					$this->setPreference('FB', Filter::postInteger('NEW_FB'));
-					Log::addConfigurationLog($this->getTitle() . ' config updated');
-				}
-				$template = new AdminTemplate;
-				return $template->pageContent();
-			default:
-				http_response_code(404);
-				break;
-		}
-	}
+  // Extend ModuleConfigInterface
+  public function modAction($mod_action) {
+    switch ($mod_action) {
+      case 'admin_config':
+        if (Filter::postBool('save') && Filter::checkCsrf()) {
+          $this->setPreference('FB', Filter::postInteger('NEW_FB'));
+          Log::addConfigurationLog($this->getTitle() . ' config updated');
+        }
+        $template = new AdminTemplate;
+        return $template->pageContent();
+      default:
+        http_response_code(404);
+        break;
+    }
+  }
 
-	// Implement ModuleConfigInterface
-	public function getConfigLink() {
-		return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin_config';
-	}
+  // Implement ModuleConfigInterface
+  public function getConfigLink() {
+    return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin_config';
+  }
 
-	// Implement ModuleMenuInterface
-	public function defaultMenuOrder() {
-		return 999;
-	}
+  // Implement ModuleMenuInterface
+  public function defaultMenuOrder() {
+    return 999;
+  }
 
-	// Implement ModuleMenuInterface
-	public function getMenu() {
-		// We don't actually have a menu - this is just a convenient "hook" to execute code at the right time during page execution
-		global $controller;
+  // Implement ModuleMenuInterface
+  public function getMenu() {
+    // We don't actually have a menu - this is just a convenient "hook" to execute code at the right time during page execution
+    global $controller;
 
-		if (WT_SCRIPT_NAME === 'branches.php' && Filter::get('surname') !== "") {
+    if (WT_SCRIPT_NAME === 'branches.php' && Filter::get('surname') !== "") {
 
-			echo $this->includeCss();
+      echo $this->includeCss();
 
-			$controller
-				->addExternalJavaScript($this->directory . '/js/jquery.treeview.js')
-				->addInlineJavaScript('
+      $controller
+          ->addExternalJavaScript($this->directory . '/js/jquery.treeview.js')
+          ->addInlineJavaScript('
 					jQuery(".wt-main-container form")
 						.after("<div id=\"treecontrol\"><a href=\"#\">' . I18N::translate('Collapse all') . '</a> | <a href=\"#\">' . I18N::translate('Expand all') . '</a></div>")
 						.after("<div class=\"loading-image\"></div>");
@@ -111,13 +111,13 @@ class FancyBranchesModule extends AbstractModule implements ModuleConfigInterfac
 					jQuery("li[title=\"' . I18N::translate('Private') . '\"]").hide();
 				');
 
-			if ($this->getPreference('FB')) {
-				$controller->addInlineJavaScript('
+      if ($this->getPreference('FB')) {
+        $controller->addInlineJavaScript('
 					jQuery("#branch-list, #branch-list ul, #branch-list li").addClass("aboville");
 				');
-			}
+      }
 
-			$controller->addInlineJavaScript('
+      $controller->addInlineJavaScript('
 				jQuery("#branch-list").treeview({
 					collapsed: true,
 					animated: "slow",
@@ -126,37 +126,37 @@ class FancyBranchesModule extends AbstractModule implements ModuleConfigInterfac
 				jQuery("#branch-list").show();
 				jQuery(".loading-image").hide();
 			');
-		}
-		return null;
-	}
+    }
+    return null;
+  }
 
-	 /**
-	 * Default Fancy script used in all Fancy modules with css
-	 *
-	 * Use plain javascript to include the stylesheet(s) in the header and set the theme class on the body
-	 * Use a theme class on the body to simply reference it by css
-	 *
-	 * The code to place the stylesheet in the header renders quicker than the default webtrees solution
-	 * because we do not have to wait until the page is fully loaded
-	 *
-	 * Replace all classnames on the body to prevent double theme classes set by multiple fancy modules
-	 *
-	 * @return javascript
-	 */
-	protected function includeCss() {
-		return
-			'<script>
-				var newSheet=document.createElement("link");
-				newSheet.setAttribute("rel","stylesheet");
-				newSheet.setAttribute("type","text/css");
-				newSheet.setAttribute("href","' . $this->directory . '/css/style.css");
-				document.getElementsByTagName("head")[0].appendChild(newSheet);
+  /**
+   * Default Fancy script used in all Fancy modules with css
+   *
+   * Use plain javascript to include the stylesheet(s) in the header and set the theme class on the body
+   * Use a theme class on the body to simply reference it by css
+   *
+   * The code to place the stylesheet in the header renders quicker than the default webtrees solution
+   * because we do not have to wait until the page is fully loaded
+   *
+   * Replace all classnames on the body to prevent double theme classes set by multiple fancy modules
+   *
+   * @return javascript
+   */
+  protected function includeCss() {
+    return
+        '<script>
+          var newSheet=document.createElement("link");
+          newSheet.setAttribute("rel","stylesheet");
+          newSheet.setAttribute("type","text/css");
+          newSheet.setAttribute("href","' . $this->directory . '/css/style.css");
+          document.getElementsByTagName("head")[0].appendChild(newSheet);
 
-				window.addEventListener("load", function () {
-						document.body.className = "wt-global theme-' . Theme::theme()->themeId() . '";
-				}, false);
-			</script>';
-	}
+          window.addEventListener("load", function () {
+              document.body.className = "wt-global theme-' . Theme::theme()->themeId() . '";
+          }, false);
+        </script>';
+  }
 
 }
 
