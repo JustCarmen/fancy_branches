9eca4dd31b129c4238b33fd9f0b7ad3e7b19033a
JustCarmen <carmen@justcarmen.nl>
2017-12-10 20:42:24 +0100

    Fix uses

--- module.php
+++ module.php
@@ -17,6 +17,7 @@ namespace JustCarmen\WebtreesAddOns\FancyBranches;
 
 use Composer\Autoload\ClassLoader;
 use Fisharebest\Webtrees\Filter;
+use Fisharebest\Webtrees\Html;
 use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Log;
 use Fisharebest\Webtrees\Module\AbstractModule;
